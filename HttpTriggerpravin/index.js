0ab0d5efea2b513d5d9a28e546fee1af6c999038
niluashy <52160806+niluashy@users.noreply.github.com>
2019-07-04 17:59:40 +0530

    Update index.js

--- HttpTriggerpravin/index.js
+++ HttpTriggerpravin/index.js
@@ -10,7 +10,7 @@ module.exports = async function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request bodyies"
+            body: "Please pass a name on the query string or in the request aws Cloud "
         };
     }
 };
